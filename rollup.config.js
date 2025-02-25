424806dfadffd8336c8ca64f766078e5c2108413
justinomps <63919879+justinomps@users.noreply.github.com>
2025-02-25 18:00:38 -0500

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/chain-home-radar.js',
+    format: 'iife',
+    name: 'ChainHomeRadar'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-react']
+    }),
+    terser()
+  ]
+};
